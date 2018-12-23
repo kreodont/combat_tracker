71fcb3abed550a0cf8346689f2bc5d9b14566d6a
kreodont <dimasanov@gmail.com>
2018-12-23 23:20:53 +0300

    Working on series test

--- temp/__init__.py
+++ temp/__init__.py
@@ -1,10 +1,11 @@
 import typing
 import datetime
+import uuid
 
 
 class Value(typing.NamedTuple):
     name: str
-    id: str
+    id: str = uuid.uuid4()
     value: typing.Any = 0
     actions_sequence: list = []
     subscribers: list = []
@@ -18,26 +19,37 @@ class Value(typing.NamedTuple):
 
 
 class Action(typing.NamedTuple):
-    name: str
-    id: str
+    id: str = uuid.uuid4()
     time: datetime.datetime = datetime.datetime.now()
+    name: str = f'Action {id} at {time}'
     previous_value: typing.Optional[Value] = None
     actual_value: typing.Optional[Value] = None
-    function: typing.Callable = lambda x: x
-    rollback: typing.Callable = lambda x: x
+    function: typing.Callable[..., Value] = None
+    rollback: typing.Callable[..., Value] = None
     short_description: str = f'Changing value from {previous_value} to {actual_value}'
     full_description: str = short_description
     visibility_level: str = 'visible'
     next_action: typing.Optional['Action'] = None
     previous_action: typing.Optional['Action'] = None
 
+    # def change_value(self, value: Value):
+    #     return change_value(value, self)
 
-def change_value(value: Value, action: Action):
-    value.actions_sequence.append(action)
-    action.function()
+
+def change_value(value_to_change: Value, action_to_perform: Action):
+    new_actions_list = value_to_change.actions_sequence + [action_to_perform]
+    value_to_change = value_to_change._replace(actions_sequence=new_actions_list)
+    action_to_perform = action_to_perform._replace(
+            previous_value=value_to_change.value,
+            actual_value=action_to_perform.function(value_to_change))
+    return action_to_perform
 
 
 def roll_back_action(value: Value, action_id: typing.Optional[str] = None):
     if not value.actions_sequence:
         raise Exception(f'Value {value} action {action_id} was tried to rollback, '
                         f'but there were no actions in this value')
+
+
+if __name__ == '__main__':
+    pass
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from temp import Action, Value
+from temp import Action, Value, change_value
 
 
 def test_create_empty_action():
@@ -9,3 +9,40 @@ def test_create_empty_action():
 def test_create_empty_value():
     value = Value(name='test_value', id='2')
     assert value.full_description == '0'
+
+
+def test_change_integer_value_from_0_to_10():
+    value = Value(name='test_value', value=0)
+
+    def f(v: Value) -> Value:
+        v = v._replace(value=10)
+        return v
+
+    action = Action(name='test_action', id='1', function=f)
+    action = change_value(value, action)
+    assert action.actual_value.value == 10
+    assert len(action.actual_value.actions_sequence) == 1
+
+
+def test_series_of_changes():
+    value = Value(name='test_value', value=0)
+    # print(f'Len: {len(value.actions_sequence)}\n')
+
+    def change_to_11(v: Value) -> Value:
+        v = v._replace(value=11)
+        return v
+
+    def change_to_12(v: Value) -> Value:
+        v = v._replace(value=12)
+        return v
+
+    action = Action(function=change_to_11)
+    action = change_value(value, action)
+    assert action.actual_value.value == 11
+    assert len(action.actual_value.actions_sequence) == 1
+
+    action = Action(function=change_to_12)
+    action = change_value(value, action)
+    assert action.actual_value.value == 12
+    assert len(action.actual_value.actions_sequence) == 2
+
