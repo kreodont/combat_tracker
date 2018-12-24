4f5b59dda8d661e980df0048fa09b69b6b78665e
dima <dmitry@retailnext.net>
2018-12-24 14:30:43 +0300

    Test for accessing previous value

--- tests/test.py
+++ tests/test.py
@@ -26,7 +26,6 @@ def test_change_integer_value_from_0_to_10():
 
 def test_series_of_changes():
     value = Value(name='test_value', value=0)
-    # print(f'Len: {len(value.actions_sequence)}\n')
 
     def change_to_11(v: Value) -> Value:
         v = v._replace(value=11)
@@ -38,7 +37,7 @@ def test_series_of_changes():
 
     action = Action(function=change_to_11)
     new_value = change_value(value, action)
-    print(new_value.value)
+
     assert new_value.value == 11
     assert len(new_value.actions_sequence) == 1
 
@@ -57,3 +56,13 @@ def test_change_from_action():
     action = Action(function=change_to_100)
     value = action.change_value(value)
     assert value.value == 100
+
+
+def test_that_previous_value_is_still_accessible():
+    def change_to_55(v: Value) -> Value:
+        v = v._replace(value=55)
+        return v
+    value = Value(name='some', value=4)
+    value = Action(function=change_to_55).change_value(value)
+    assert value.value == 55
+    assert value.actions_sequence[-1].previous_value.value == 4
